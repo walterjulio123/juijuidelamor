7fa5f4c20e98bbc8ccf9d68f64ffc347a147e0a8
walterjulio123 <walterjulio123@gmail.com>
2017-05-07 16:03:31 -0500

    Commit juijui V3.1

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 //Creamos archivo server.js para poner el codigo del servidor:
 var express = require('express');
 var app = express();
-var server = require('http').createserver(app);  
+//var server = require('http').createserver(app);  
 var io = require('socket.io').listen(server);
-//var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 var users = [];
 var connections = [];
-
+/*
 server.listen(process.env.PORT || 3000);
 console.log('Server is running...');
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
-/*
+*/
+
 app.get('/', function (req, res) {
     res.send("Juijui del amorrrr y la felicidaaaaad en juijuidelamor V3.1!");
 });
@@ -21,4 +22,3 @@ app.get('/', function (req, res) {
 var server = app.listen(port, function () {
     console.log('node server is just fine! and running on port - ' + port);
 });
-*/
