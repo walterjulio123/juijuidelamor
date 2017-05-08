f3dc69ef94adb41e0a331cfe0b5de131561088ca
walterjulio123 <walterjulio123@gmail.com>
2017-05-07 20:34:36 -0500

    Commit juijui V3.1

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/',function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
 */
+app.use(express.static('public'));
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
